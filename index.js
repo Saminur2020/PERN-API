30b259a5d0a64710bf3d18140a6bbf982fd748fe
Samanur Miah <samanurmiah@samanurs-mbp.mynetworksettings.com>
2023-12-10 18:56:03 -0500

    packages installed, queries and index created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = requires ('express')
+const app = express ()
+const PORT = 9001
+
+app.get('/' , (req , res) => {
+
+    res.send("Testing from the server")
+})
+
+app.listen(PORT, () => {
+    console.log("The app is running on port ${PORT}.")
+})
